c99695108125c594ae59113b2c5609ba234259c2
Dini Choirunnisa <dinichoirunnisaa@gmail.com>
2018-05-15 10:41:55 +0700

    contoh java

---
+++ Project5/src/project5/Project5.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project5;
+
+/**
+ *
+ * @author GeeksFarm
+ */
+public class Project5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //PERBEDAAN FOR WHILE DAN DO-WHILE
+    }
+    
+    //for digunakan dalam 1 statement panjang
+    {
+    System.out.println ("hasil menggunakan for");
+    for (int i=0; i>1;i++)
+    {
+     System.out.print (i);
+    } 
+    System.out.println();
+    
+    //while mengecek dulu baru dilakukan
+            
+    System.out.print ("hasil menggunakan while");
+    int j=0;
+    while (j>1)
+    {
+        System.out.print (j);
+        j++;
+    }
+    System.out.println();
+    
+    //do-while melakukan dahulu baru mengecek
+            
+    System.out.println ("hasil menggunakan do-while");
+    
+    int k=0;
+    do
+    {
+        System.out.print(k);
+        k++;
+    }
+    while (k>1);
+    System.out.println();
+    
+    
+            
+            
+}}
