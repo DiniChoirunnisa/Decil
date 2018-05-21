a19c2853bd9d61259b82e1745b28d359d7fa5d01
Dini Choirunnisa <dinichoirunnisaa@gmail.com>
2018-05-21 13:38:34 +0700

    edit

---
+++ Project14/src/project14/Project14.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project14;
+
+/**
+ *
+ * @author GeeksFarm
+ */
+public class Project14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //public static void ini berguna untuk menuliskan method atau dengan kata lain adalah
+    //mengelompokkan suatu koding agar bisa digunakan berkali2
+    public static void garis (){
+        System.out.println ("=================");
+    }         
+    public static void main(String[] args) {
+    //MEMPELAJARI METHOD
+        garis();
+        System.out.println ("Judul");
+        garis();
+        System.out.println("Sub Judul");
+        garis();
+    }
+    
+}
